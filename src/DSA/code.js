a5f042eb03a3b4dc29dcb9c9a92494e4718d8d14
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-23 12:53:08 +0530

    two sum

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -1,7 +1,23 @@
 // TWO SUM
+// 3
+// 4 9
+// 2 7 11 15
+// 5 10
+// 1 2 3 5 5
+// 2 100
+// 48 49
 
 let l=0;
 let r=n-1;
 while(l<r){
-    
+    if(arr1[l]+arr2[r]===k){
+        return l+" "+r
+    }
+    else if(arr1[l]+arr2[r]>k){
+        r--;
+    }
+    else{
+        l++;
+    }
 }
+return -1 -1;
