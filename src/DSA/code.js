df5985c23b5619d0191ffc0568cd0724f7c1c575
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-24 12:10:46 +0530

    element is larger than its neighbour

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -30,10 +30,10 @@ return -1 -1;
 
 let s=0;
 let e=n-1;
-let count=0;
+let count1=0;
 while(s<e && n>=0){
     if(arr1[s]===arr2[e]){
-        count++;
+        count1++;
         s++;
         e--;
     }
@@ -45,3 +45,27 @@ while(s<e && n>=0){
     }
 }
 console.log(count);
+
+//element is larger than its neighbour
+// 8
+// 1 2 3 4 2 1 6 5
+
+let count2=0;
+for(let i=0;i<n;i++){
+    if(i===0){
+        if(arr[i]>arr[i+1]){
+            count2++
+        }
+    }
+    else if(i===n-1){
+        if(arr[i]>arr[i-1]){
+            count2++;
+        }
+    }
+    else if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+        count2++;
+    }
+}
+console.log(count2);
+
+
