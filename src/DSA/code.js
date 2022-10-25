09bca2fa5069996360415141a33c74d80e29058e
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-25 13:50:05 +0530

    full question sne modified

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -134,6 +134,10 @@ else if(k===3){
 }
 
 // Reduce String
+// You can perform one operation on this string in which you can selects a pair of adjacent lowercase letters that match, and delete them.
+// For instance, the string aab could be shortened to b in one operation.
+// Your task is to delete as many characters as possible using this method and print the resulting string. If the final string is empty, print "Empty String" (without quotes).
+// Please note that characters can be deleted only if they form a pair and are same(i.e from aaa we can only delete 2 a's and will be left with a single a).
 // str= aaabccddd
 
 let stack1=[];
