55ede8efb8e87f8b506aafa28890b5133617efee
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-26 14:12:25 +0530

    fs

--- src/Node js/index.js
+++ src/Node js/index.js
@@ -1,10 +1,18 @@
 // const http= require("http");
 // const data= require("./data")
 
-
+// procss use fs
 const fs = require("fs");
 const input= process.argv
-fs.writeFileSync(input[2],input[3])
+if(input[2]==="add"){
+    fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]==="remove"){
+    fs.unlinkSync(input[3])
+}
+else{
+    console.log("wrong");
+}
 
 
 // http.createServer((req,res)=>{
