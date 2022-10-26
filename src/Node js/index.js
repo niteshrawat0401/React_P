3aea58ec42f82fc107e2cfd292c5052f46fee24b
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-26 14:02:36 +0530

    use fs

--- src/Node js/index.js
+++ src/Node js/index.js
@@ -1,7 +1,14 @@
-const http= require("http");
-const data= require("./data")
-http.createServer((req,res)=>{
-res.writeHead(200, {"Content-Type": "application\json"});
-res.write(JSON.stringify(data));
-res.end();
-}).listen(5000)
+// const http= require("http");
+// const data= require("./data")
+
+
+const fs = require("fs");
+const input= process.argv
+fs.writeFileSync(input[2],input[3])
+
+
+// http.createServer((req,res)=>{
+// res.writeHead(200, {"Content-Type": "application\json"});
+// res.write(JSON.stringify(data));
+// res.end();
+// }).listen(5000)
