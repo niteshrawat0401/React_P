ea376c7849c3417cb1703d300b5a248a50bc25ec
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-27 11:40:16 +0530

    read file

--- src/Node js/index.js
+++ src/Node js/index.js
@@ -1,22 +1,39 @@
 // const http= require("http");
 // const data= require("./data")
+const fs= require("fs");
+const path= require("path");
+const dirPath= path.join(__dirname, "crud");
+const filePath = `${dirPath}/apple.txt`;
 
-// procss use fs
-const fs = require("fs");
-const input= process.argv
-if(input[2]==="add"){
-    fs.writeFileSync(input[3],input[4]);
-}
-else if(input[2]==="remove"){
-    fs.unlinkSync(input[3])
-}
-else{
-    console.log("wrong");
-}
+// File Read
+// fs.readFile(filePath, "utf8", (err, item)=>{
+//     console.log(item);
+// })
 
 
+
+
+// Procss Use fs
+// const fs = require("fs");
+// const input= process.argv
+// if(input[2]==="add"){
+//     fs.writeFileSync(input[3],input[4]);
+// }
+// else if(input[2]==="remove"){
+//     fs.unlinkSync(input[3])
+// }
+// else{
+//     console.log("wrong");
+// }
+
+
+
+  
+
+
+// Create Server
 // http.createServer((req,res)=>{
 // res.writeHead(200, {"Content-Type": "application\json"});
 // res.write(JSON.stringify(data));
-// res.end();
+// res.end();z   
 // }).listen(5000)
