3f45f965b8af61a869bc6f6929a98bcef7d305cb
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-06 15:46:40 +0530

    Plus One

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -524,3 +524,21 @@ while (start < end) {t
     }
 }
 return start;
+
+// Plus One
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+for(let i=digits.length-1;i>=0;i--){
+    digits[i]++;
+    if(digits[i]> 9){
+        digits[i]= 0;
+    }
+    else
+     return digits;
+}
+    digits.unshift(1)
+    return digits;
