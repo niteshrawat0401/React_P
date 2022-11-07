45fbe9368540cbf843732f54173d15958473b605
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-07 11:29:41 +0530

    Search Insert Position

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -542,3 +542,28 @@ for(let i=digits.length-1;i>=0;i--){
 }
     digits.unshift(1)
     return digits;
+
+
+// Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+    let l6=0;
+    let r6=nums.length-1;
+    while(l6<=r6){
+        mid= Math.floor(l6+(r6-l6)/2);
+        if(nums[mid]===target){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            r6=mid-1;
+        }
+        else{
+            l6=mid+1
+        }
+    }
+    return l;
