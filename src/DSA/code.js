33c40f0518326f0f2cfe33ca56a7f0e1ffae8c30
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-09 12:07:58 +0530

    Squares of a Sorted Array

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -833,3 +833,17 @@ for(let i=0;i<n;i++){
     }
 }
 return true;
+
+// Squares of a Sorted Array
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+
+let pro=0;
+for(let i=0;i<n;i++){
+    pro.push(nums[i]*nums[i]);
+}
+pro.sort((a,b)=> a-b);
+return pro;
