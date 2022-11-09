b6b22b5d9c8a4c400a1cf92b06a1d453254397b4
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-10 00:46:39 +0530

    add

--- src/Node js/server.js
+++ src/Node js/server.js
@@ -2,3 +2,8 @@ var buf= Buffer.alloc(10);
 buf.write("hello");
 console.log(buf);
 console.log(buf.toString());
+
+
+var buf2= Buffer.from("Wellcome");
+console.log(buf2);
+console.log(buf2.toJSON());
