4a107f73f8cf5f3424783c32dbfd43d91064b9cc
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-09 11:45:38 +0530

    Find Pivot Index

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -774,3 +774,25 @@ function palindrome(s,left2,right2){
     }
 }
 return true;
+
+// Find Pivot Index
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+let sum= nums.reduce((a,b)=> a+b);
+let left3=0;
+let right3=0;
+for(let i=0;i<n;i++){
+    right3=sum-left3-nums[i];
+    if(left3==right3){
+        return i;
+    }
+    left3+= nums[i];
+}
+return -1;
