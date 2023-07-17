fe7ca83fae7142c344499f02afd02c4e3d71a34c
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-17 10:52:59 +0530

    most occuring word

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -1002,3 +1002,27 @@ var lengthOfLongestSubstring = function(s) {
     }
     return -1;
 };
+
+function maximumOccurring(str){
+    let obj = {};
+    for(let i=0;i<str.length;i++){
+        if(obj[str[i]] === undefined){
+            obj[str[i]] = 1;
+        }
+        else{
+            obj[str[i]]++;
+        }
+    }
+
+    let max = -Infinity;
+    for(let key in obj){
+        if(obj[key] > max){
+            max = obj[key]
+            num = key
+        }
+    }
+    console.log(num);
+    
+}
+let string = "geeksforgeeks";
+maximumOccurring(string)
