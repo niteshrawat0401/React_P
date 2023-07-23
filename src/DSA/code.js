170455c9381b8028d34032669f9ae3d23f7968e6
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-23 20:41:50 +0530

    firstPalindrome from array

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -1052,3 +1052,37 @@ var rotateString = function(s, goal) {
         return false;
 
 };
+
+// Input: words = ["abc","car","ada","racecar","cool"] // Output: "ada"
+// Explanation: The first string that is palindromic is "ada".
+// Note that "racecar" is also palindromic, but it is not the first.
+// Example 2:
+
+// Input: words = ["notapalindrome","racecar"] // Output: "racecar"
+// Explanation: The first and only string that is palindromic is "racecar".
+// Example 3:
+
+// Input: words = ["def","ghi"] // Output: ""
+// Explanation: There are no palindromic strings, so the empty string is returned.
+var firstPalindrome = function(words) {
+    for(let i=0;i<words.length;i++){
+        let j = words[i];
+            let x = palindrome(j)
+            if(x === true){
+                return j
+            }
+    }
+        return ""
+}
+    function palindrome(j){
+    let l =0;
+    let r =j.length-1
+    while( l<r){
+        if(j[l] !== j[r]){
+            return false
+        }
+        l++;
+        r--;
+    }
+    return true
+}
