2a4510644673dcdfd3fd0bbeec01c3dda05d7f91
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-26 13:28:28 +0530

    fizz buzz

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -1114,3 +1114,32 @@ var groupAnagrams = function(strs) {
     }
     return arr
 };
+
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+// Example 2:
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+// Example 3:
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+var fizzBuzz = function(n) {
+    let arr = [];
+    for(let i=1;i<=n;i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            arr.push("FizzBuzz")
+        }
+        else if(i % 3 == 0){
+            arr.push("Fizz")
+        }
+        else if(i % 5 == 0){
+            arr.push("Buzz")
+        }
+        else{
+            let a = i.toString()
+            arr.push(a)
+        }
+    }
+    return arr
+};
