a9681f0a7a071060eef4934834b689261f646bc3
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-08-05 13:22:58 +0530

    countbits

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -1229,3 +1229,36 @@ let arr = [6,5,6,12,18,5,18,9]
       }
       return arr
  };
+
+
+//  Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+ Example 1:
+//  Input: n = 2
+//  Output: [0,1,1]
+//  Explanation:
+//  0 --> 0
+//  1 --> 1
+//  2 --> 10
+//  Example 2:
+//  Input: n = 5
+//  Output: [0,1,1,2,1,2]
+//  Explanation:
+//  0 --> 0
+//  1 --> 1
+//  2 --> 10
+//  3 --> 11
+//  4 --> 100
+//  5 --> 101
+ var countBits = function(n) {
+    const ans = new Array(n + 1).fill(0);
+    console.log(ans)
+   for (let i = 0; i <= n; i++) {
+    if (i % 2 === 0) {
+      ans[i] = ans[i / 2];
+
+    } else {
+      ans[i] = ans[Math.floor(i / 2)] + 1;
+    }
+  }
+  return ans;
+};
