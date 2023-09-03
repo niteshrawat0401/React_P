3ec6b0944e79b3a9ff26c21342ad1e086d921c95
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-09-03 13:32:05 +0530

    reverse bit

--- src/DSA/code.js
+++ src/DSA/code.js
@@ -1577,3 +1577,22 @@ if (Array.isArray(myVariable)) {
 } else {
     console.log("myVariable is not an array.");
 }
+
+// Reverse Bits
+// Example 1:
+// Input: n = 00000010100101000001111010011100
+// Output:    964176192 (00111001011110000010100101000000)
+// Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+// Example 2:
+// Input: n = 11111111111111111111111111111101
+// Output:   3221225471 (10111111111111111111111111111111)
+// Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+function reverseBit(){
+  let res = n.toString().split("");
+  let num = 32 - res.length;
+  for(let i=0;i<num;i++){
+    res.unshift("0")
+  }
+  let ans = res.reverse().join("");
+  return parseInt(ans, 2);
+}
