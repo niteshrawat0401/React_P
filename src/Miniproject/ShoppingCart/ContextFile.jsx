62b74440847b0a2bee2111e9fee2ba44ad90efd0
niteshrawat0401 <niteshrawat0401@gmail.com>
2024-10-13 12:00:41 +0530

    context file for cart

--- src/Miniproject/ShoppingCart/ContextFile.jsx
+++ src/Miniproject/ShoppingCart/ContextFile.jsx
@@ -1,21 +1,62 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const UseContext = createContext();
 
 export const Context = ({ children }) => {
-  const [cart, setCart] = useState(4);
-  const [productData, setProductData] = useState(products);
+    const [cart, setCart] = useState([]);
+    const [productData, setProductData] = useState(products);
+  const [updateQuan, setUpdateQuan] = useState([]);
+
+
+  useEffect(()=>{
+    setUpdateQuan(cart.map(item => ({name: item?.name, quantity: 1, price: item.price })));
+  },[cart])
+
+  const handleIncQuantity = (ele,i)=>{
+    let newUpdateQuan = updateQuan.map((item,index)=> {
+        if(index === i){
+            const newQuantity = item.quantity + 1;
+           return { ...item,quantity: newQuantity,price:  ele?.price * newQuantity}
+        }
+        return item
+    })
+    setUpdateQuan(newUpdateQuan)
+}
+  const handleDecQuantity = (ele,i)=>{
+
+    const newUpdateQuan = updateQuan.map((item, index)=>{
+        if(index === i){
+            const newQuantity = item?.quantity - 1;
+            return {...item, quantity: newQuantity, price: ele?.price * newQuantity}
+        }
+         return item
+    })
+    setUpdateQuan(newUpdateQuan)
+  }
+
+  const handleDelete = (ele,i)=>{
+    let deleteItem = cart.filter((item,index)=>{
+        return index !== i && item
+    })
+    setCart(deleteItem)
+}
+
 
   return (
-    <UseContext.Provider value={{ cart, setCart, productData }}>
+    <UseContext.Provider value={{ 
+        cart, setCart, 
+        productData, 
+        updateQuan, setUpdateQuan,
+        handleIncQuantity, handleDecQuantity, handleDelete
+        }}>
       {children}
     </UseContext.Provider>
   );
 };
 
 const products = [
-  { name: "iPhone 15 Pro Max", price: 129999 },
-  { name: "Samsung Galaxy S24 Ultra", price: 109999 },
+  { name: "iPhone 15 Pro Max", price: 2 },
+  { name: "Samsung Galaxy S24 Ultra", price: 3 },
   { name: "Google Pixel 8 Pro", price: 89999 },
   { name: "OnePlus 12 Pro", price: 69999 },
   { name: "Xiaomi 14 Pro", price: 59999 },
