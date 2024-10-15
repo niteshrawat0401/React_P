dbb28e596dbb808363489411eb851c6c9ca0ebd5
niteshrawat0401 <niteshrawat0401@gmail.com>
2024-10-15 11:54:22 +0530

    stopt watch and remove uneccessary component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { lazy, Suspense } from "react";
-// import { Display } from './redux-prac/display/Display';
 import Button from "./Button/Button";
 import { Toggle } from "./Button/Toggle";
 import Serach from "./Searchfilter/Serach";
 import BookData from "./Searchfilter/db.json";
-import { Todo } from "./Todo/pages/Todo";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Scooty/Home";
-import {Display} from "./Scooty/Display"
-import { Puppy } from "./puppy love/Puppy";
 import { Puppyname } from "./puppy love/Puppyname";
 import { Pagination } from "./Pagination/Pagination";
 import { Landing } from "./Cryptocurrency_Pagi/Landing";
 import { Infinte } from "./Infinte scroll/Infinte";
 import { UseMemo } from "./Button/UseMemo";
-import { StudentDetails } from "./table/StudentDetails";
 import { Usebtn } from "./Button/Usebtn";
 import Useref from "./Button/Useref";
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -30,6 +25,7 @@ import Paginate from "./Miniproject/Pagination/Paginate";
 import Product from "./Miniproject/ShoppingCart/Product";
 import { Context } from "./Miniproject/ShoppingCart/ContextFile";
 import CartPage from "./Miniproject/ShoppingCart/CartPage";
+import Time from "./Miniproject/StopWatch/Time";
 // import { LimitedData } from "./Customhook/LimitedData";
 
 const LimitedData = lazy(()=> import('./Customhook/LimitedData'));
@@ -39,8 +35,6 @@ function App() {
   return (
     <div className="App">
       <Suspense fallback={<h1>...loading</h1>}>
-      {/* <StudentDetails/> */}
-      {/* <Display/> */}
       {/* <Button/> */}
       {/* <Toggle/> */}
       {/* <UseMemo/> */}
@@ -48,18 +42,9 @@ function App() {
       {/* <Useref/> */}
       {/* <Serach placeholder="Enter a Book Name..." data={BookData}/> */}
       {/* <Infinte/> */}
-      {/* <Todo/> */}
       {/* <Home/> */}
      {/* <Pagination/> */}
      {/* <Landing/> */}
-      {/* <Routes>
-        <Route path="/" element={<Puppy/>} />
-        <Route path="/puppy/:name" element={<Puppyname/>} />
-      </Routes> */}
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dis/:id" element={<Display />} />
-      </Routes> */}
       {/* <GoogleOAuthProvider clientId={clientId}>
         <AccountProvider>
         <Main/>
@@ -73,14 +58,13 @@ function App() {
 
 
         {/* <Paginate/> */}
-        <Context>
+        <Time/>
+        {/* <Context>
         <Routes>
           <Route path="/product" element={<Product/>}/>
           <Route path="/cart" element={<CartPage/>}/>
         </Routes>
-        </Context>
-        {/* <Product/>
-        <CartPage/> */}
+        </Context> */}
         </Suspense>
     </div>
   );
